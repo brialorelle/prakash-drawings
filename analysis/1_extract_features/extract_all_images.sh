19797c08596778ef3adc01e5f5588a69e5ef9e49
Bria Long <brialorelle@gmail.com>
2021-04-09 17:45:27 -0700

    Update extract_all_images.sh

--- analysis/1_extract_features/extract_all_images.sh
+++ analysis/1_extract_features/extract_all_images.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-python extract_features.py --data='/data3/bria/kiddraw_datasets/photodraw2_020519/' --layer_ind=6 --cohort='kid' --ext="png" --spatial_avg=True --dataset='photodraw2_020519'
 
-python extract_features.py --data='/home/bria/devphotodraw/data/object_drawings/' --layer_ind=6 --cohort='kid' --ext="png" --spatial_avg=True --dataset='photodraw_compiled'
+python extract_features.py --data='/home/bria/prakash-drawings/data/obj_drawings/' --layer_ind=6 --cohort='kid' --ext="png" --spatial_avg=True --dataset='prakash'
