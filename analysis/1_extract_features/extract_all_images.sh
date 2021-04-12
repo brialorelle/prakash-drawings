2de97d975ecd5195defbf10aca68860382fbf337
Bria Long <brialorelle@gmail.com>
2021-04-12 10:47:20 -0700

    change output size info, add back code to remove alpha channel

--- analysis/1_extract_features/extract_all_images.sh
+++ analysis/1_extract_features/extract_all_images.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-python extract_features.py --data='/home/bria/prakash-drawings/data/obj_drawings/' --layer_ind=6 --cohort='kid' --ext="png" --spatial_avg=True --dataset='prakash'
+python extract_features.py --data='/home/bria/prakash-drawings/data/drawings/obj_drawings/' --layer_ind=6 --cohort='kid' --ext="png" --spatial_avg=True --dataset='prakash'
